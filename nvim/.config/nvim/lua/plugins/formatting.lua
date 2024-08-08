850a4aec4594e4a2540a7a9ca79b86db91291b38
zeekcheung <zeekcheung@gmail.com>
2024-08-08 17:15:41 +0800

    chore(nvim): update

--- nvim/.config/nvim/lua/plugins/formatting.lua
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -3,7 +3,10 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
+        zsh = { "shfmt" },
         css = { "prettier" },
+        json = { lsp_format = "first" },
+        jsonc = { lsp_format = "first" },
       },
     },
   },
