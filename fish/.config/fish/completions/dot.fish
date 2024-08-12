19746349bef3d93abdb7dfdd64b16fab31309b19
zeekcheung <zeekcheung@gmail.com>
2024-08-12 23:44:02 +0800

    feat(fish): open file with dot command

--- fish/.config/fish/completions/dot.fish
+++ fish/.config/fish/completions/dot.fish
@@ -1,2 +1,5 @@
-# Define completion for the dot function
-complete --no-files --command dot --arguments "(ls -d ~/.dotfiles/*/ | cut -f 5 -d '/')" --description "Package dotfiles to search"
+set -l packages (fd . --max-depth=1 --min-depth=1 ~/.dotfiles/ | string trim --right --chars=/ | cut -d '/' -f 5)
+
+for package in $packages
+    complete --no-files --command dot --arguments $package --description "$package dotfiles"
+end
--- fish/.config/fish/conf.d/dot.fish
+++ fish/.config/fish/conf.d/dot.fish
@@ -2,12 +2,12 @@ function fzf_filename_first --description "Fuzzy find with filename_first format
     fzf --delimiter / --with-nth -2,-1 --preview 'echo {}\n && fzf-preview {}'
 end
 
-function ff --description "Fuzzy find files and process the result"
+function f --description "Fuzzy find files and process the result"
     fd . $argv | string trim --right --chars=/ | fzf_filename_first
 end
 
 function vf --description "Fuzzy find a file and open with the editor specified in \$EDITOR"
-    set -l file (ff $argv)
+    set -l file (f $argv)
     if test -n "$file"
         $EDITOR "$file"
     end
@@ -17,7 +17,7 @@ function dot --description "Fuzzy find a dotfile and open with the editor specif
     if test (count $argv) -eq 0
         vf -H ~/.dotfiles/
     else
-        set -l subdir $argv[1]
-        vf -H ~/.dotfiles/$subdir/
+        set -l target ~/.dotfiles/$argv[1]
+        test -d "$target" && vf -H "$target" || $EDITOR "$target"
     end
 end
