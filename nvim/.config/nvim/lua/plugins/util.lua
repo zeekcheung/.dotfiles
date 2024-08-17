13e2547917bf6a4f5a30d2be6593884a3946298c
zeekcheung <zeekcheung@gmail.com>
2024-08-17 21:49:34 +0800

    chore(tmux): update

--- nvim/.config/nvim/lua/plugins/util.lua
+++ nvim/.config/nvim/lua/plugins/util.lua
@@ -1,3 +1,12 @@
 return {
-  { "christoomey/vim-tmux-navigator", lazy = false },
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    },
+  },
 }
