421917a1ec6c70f25e58cef4aeea7a4936a2e94d
zeekcheung <zeekcheung@gmail.com>
2024-09-27 13:33:37 +0800

    feat: gruvbox-material

--- fish/.config/fish/conf.d/env.fish
+++ fish/.config/fish/conf.d/env.fish
@@ -37,20 +37,6 @@ set -gx FZF_DEFAULT_OPTS "
 --preview='echo {}\n && fzf-preview {}'
 --preview-window=right,60%
 --color=bg+:-1
---color=border:#589ed7 
---color=fg:#c8d3f5 
---color=gutter:#1e2030 
---color=header:#ff966c 
---color=hl+:#65bcff 
---color=hl:#65bcff 
---color=info:#545c7e 
---color=marker:#ff007c 
---color=pointer:#ff007c 
---color=prompt:#65bcff 
---color=query:#c8d3f5:regular 
---color=scrollbar:#589ed7 
---color=separator:#ff966c 
---color=spinner:#ff008c 
 "
 
 set -gx FZF_CTRL_T_OPTS "
--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -71,7 +71,8 @@ return {
   {
     "sainnhe/gruvbox-material",
     config = function()
-      vim.g.gruvbox_material_background = "hard"
+      -- vim.g.gruvbox_material_foreground = "original"
+      -- vim.g.gruvbox_material_background = "hard"
       vim.g.gruvbox_material_enable_italic = true
       vim.g.gruvbox_material_transparent_background = vim.g.transparent and 1 or 0
       vim.g.gruvbox_material_show_eob = false
