3bc478217261d1e0cbd851c71f749ef968f0bcc5
zeekcheung <zeekcheung@outlook.com>
2025-05-16 20:12:49 +0800

    feat(carapace): add carapace

--- powershell/.config/powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/.config/powershell/Microsoft.PowerShell_profile.ps1
@@ -158,5 +158,11 @@ function gst {
   git status
 }
 
+# carapace
+$env:CARAPACE_BRIDGES = 'zsh,fish,bash,inshellisense' # optional
+Set-PSReadLineOption -Colors @{ "Selection" = "`e[7m" }
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+carapace _carapace | Out-String | Invoke-Expression
+
 Invoke-Expression (&starship init powershell)
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
